305aeea759b9299fe0a9c8f0c793b2659834ff98
1dars <jumadullaeviskandar@gmail.com>
2023-12-02 13:43:11 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+a = 5621557700
+b = int(input("2 dan 10 gacaha son kiriting:"))
+if a % b:
+    print("notogri")
+else:
+    print("togri")
+
+a = 266
+b = int(input("2 dan 7 gacha kiriting"))
+if a % b:
+    print("notogri")
+else:
+    print("togri")
+
+a = int(input("olma soni:"))
+b = int(input("odam soni"))
+print(a - b)
+
